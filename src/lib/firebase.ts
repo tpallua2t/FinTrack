1ecf965c0535a5ddc62a42d0206635e68d0a63aa
tpallua2t <tpalluat2@gmail.com>
2025-06-01 02:22:26 +0200

    Fix Firebase Configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCRVlw-7v5cx7sU6WdS_n1lstt5zxyrYcg",
   authDomain: "fintrack-71f9c.firebaseapp.com",
   projectId: "fintrack-71f9c",
-  storageBucket: "fintrack-71f9c.firebasestorage.app",
+  storageBucket: "fintrack-71f9c.appspot.com",
   messagingSenderId: "700140438317",
   appId: "1:700140438317:web:f15236bc95ad5a57edc9a4",
   measurementId: "G-CBTCNP6W9P"
